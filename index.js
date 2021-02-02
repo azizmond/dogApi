95b521b6dbeb29966d798d62e87de00fccdb376e
Andres Zizmond <azizmond@gmail.com>
2021-02-02 14:50:23 -0300

    Borrado de DOM para mostrar resultado

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const URL = "https://api.thedogapi.com/v1/breeds?limit=5";
+const URL = "https://api.thedogapi.com/v1/breeds";
 let breeds = [];
 
 //Esta función trae las razas de los perros desde URL
@@ -12,17 +12,20 @@ const fetchBreeds = async (url = URL) => {
     }
 };
 
+//Muestra el mensaje de que no hay resultados
 const showAlert = () => {
     document.querySelector("#alert").innerHTML = "No dogs to show / No hay perros para mostrar";
     document.querySelector("#input").disabled = true;
-}
+};
 
+//Borra las cartas, del DOM, a las cuales se le presionó el botón de borrar
 const del = (id) => {
     document.getElementById(id).remove();
     breeds = breeds.filter((breed) => breed.id != id);
     breeds.length === 0 ? showAlert() : null;
-}
+};
 
+//Genera una carta con cada resultado
 const createCard = ({id, image, name, bred_for}) => {
     const card =
     `
@@ -30,8 +33,8 @@ const createCard = ({id, image, name, bred_for}) => {
         <div class="card mt-5 ml-3">
             <img src="${image.url}"/>
             <div class="card-body">
-                <h5 class="card-title">${name}</h5>
-                <p class="card-text">Raza: ${bred_for}</p>
+                <h5 class="card-title">Raza: ${name}</h5>
+                <p class="card-text">Criado para: ${bred_for}</p>
                 <button onClick="del(${id})" class="btn btn-danger btn-block">Borrar</button>
             </div>
         </div>
@@ -40,11 +43,13 @@ const createCard = ({id, image, name, bred_for}) => {
     document.getElementById("apiR").insertAdjacentHTML("beforeend", card);
 };
 
-//Función encargada de buscar una raza
+//Función encargada de buscar una raza y eliminar los rows
 const findBreed = () => {
     const nameToSearch = document.querySelector("#input").value;
     const searchBreed = breeds.find((breed) => breed.name.toLowerCase() === nameToSearch.toLowerCase());
-    console.log(searchBreed)
+    console.log(searchBreed);
+    const row = document.querySelector('#apiR');
+    row.innerHTML = '';
 };
 
 const loopBreeds = (breeds) => {
